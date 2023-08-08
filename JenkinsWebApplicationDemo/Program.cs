4a893d1a0171536ee9d91b42eb72b55ccd2aeabe
Md. Shafkat Haider <shafkat.haider@sslwireless.com>
2023-08-08 15:12:11 +0600

    view page added

--- JenkinsWebApplicationDemo/Program.cs
+++ JenkinsWebApplicationDemo/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +7,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMvc();
 
 var app = builder.Build();
 
@@ -15,9 +17,23 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 app.UseAuthorization();
 
-app.MapControllers();
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}");
+    endpoints.MapRazorPages();
+});
 app.Run();
+
